6578d6f555887a4ecb3463392de3531dbff3ea4b
Patrick Zhen <zhen.patrick@gmail.com>
2021-01-01 17:18:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,14 @@ board = [""," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
   puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
