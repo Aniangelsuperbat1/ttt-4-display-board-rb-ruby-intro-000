417758611ba82bd9c95013e212593f26a1b63f1e
Patrick Zhen <zhen.patrick@gmail.com>
2021-01-01 17:24:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -18,4 +18,12 @@ def display_board(board)
   puts "   |   |   "
 end
 
+def display_board(board)
+  puts " X | X | X "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
 display_board(board)
